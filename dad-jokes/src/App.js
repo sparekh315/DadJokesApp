87bc606e30495ff8177bbd5717661edc0293f419
sparekh315 <saniaparekh315@gmail.com>
2020-09-25 22:36:46 -0500

    jokes api, 10 jokes showing, and sidebar setup

---
+++ dad-jokes/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import JokeList from './Components/JokeList'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
